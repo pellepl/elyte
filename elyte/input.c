c4f3aa3a7ce78dbc22dd5e59305e23af8c222dcf
peter <peter@festinasweden.se>
2026-09-26 00:22:49 +0200

    fix longpress

--- elyte/input.c
+++ elyte/input.c
@@ -17,9 +17,9 @@ static struct
     uint32_t button_mask;
     uint32_t button_during_rotation_mask;
     uint32_t button_longpress_mask;
-    uint32_t button_press_s[_INPUT_BUTTON_COUNT];
+    tick_t button_press_tick[_INPUT_BUTTON_COUNT];
     tick_t button_release_tick[_INPUT_BUTTON_COUNT];
-    event_t ev_hw_button;
+    event_t ev_hw_button[_INPUT_BUTTON_COUNT];
     event_t ev_button;
     event_t ev_hw_scroll;
     event_t ev_scroll;
@@ -162,6 +162,18 @@ bool input_is_button_pressed(input_button_t button)
 
 static void button_event(uint32_t type, void *arg)
 {
+    input_button_t button = (input_button_t)(uintptr_t)arg;
+    if ((unsigned)button >= _INPUT_BUTTON_COUNT)
+        return;
+
+    // Edges can coalesce while this event is queued. Read the current level
+    // rather than replaying an old press after its release was dropped.
+    bool pressed = button == INPUT_BUTTON_ROTARY
+        ? !LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_2)
+        : !LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_8);
+    if (pressed == input_is_button_pressed(button))
+        return;
+    type = pressed ? EVENT_BUTTON_PRESS : EVENT_BUTTON_RELEASE;
     switch (type)
     {
     case EVENT_BUTTON_PRESS:
@@ -170,7 +182,7 @@ static void button_event(uint32_t type, void *arg)
         if (but >= _INPUT_BUTTON_COUNT)
             break;
         me.button_mask |= 1 << but;
-        me.button_press_s[but] = timer_uptime_ms() / 1000;
+        me.button_press_tick[but] = timer_now();
         break;
     }
 
@@ -211,13 +223,16 @@ static void event_handler(uint32_t type, void *arg)
     {
     case EVENT_SECOND_TICK:
     {
-        if (me.button_mask == 0)
-            break;
-        uint32_t now_s = (uint32_t)arg;
+        tick_t now = timer_now();
         for (input_button_t but = 0; but < _INPUT_BUTTON_COUNT; but++)
         {
+            // Reconcile the pin before testing the hold, even if an edge
+            // interrupt was missed or its callback is still queued.
+            button_event(0, (void *)(uintptr_t)but);
             if (!input_is_button_pressed(but) ||
-                (now_s - me.button_press_s[but] < INPUT_LONG_PRESS_SEC) ||
+                (me.button_longpress_mask & (1 << but)) != 0 ||
+                (now < me.button_press_tick[but]) ||
+                (now - me.button_press_tick[but] < TIMER_MS_TO_TICKS(INPUT_LONG_PRESS_SEC * 1000)) ||
                 ((me.button_during_rotation_mask & (1 << but)) != 0))
                 continue;
             me.button_longpress_mask |= (1 << but);
@@ -271,11 +286,11 @@ void EXTI2_TSC_IRQHandler(void)
 
         if (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_2))
         {
-            event_add_specific(&me.ev_hw_button, EVENT_BUTTON_RELEASE, (void *)INPUT_BUTTON_ROTARY, button_event);
+            event_add_specific(&me.ev_hw_button[INPUT_BUTTON_ROTARY], EVENT_BUTTON_RELEASE, (void *)INPUT_BUTTON_ROTARY, button_event);
         }
         else
         {
-            event_add_specific(&me.ev_hw_button, EVENT_BUTTON_PRESS, (void *)INPUT_BUTTON_ROTARY, button_event);
+            event_add_specific(&me.ev_hw_button[INPUT_BUTTON_ROTARY], EVENT_BUTTON_PRESS, (void *)INPUT_BUTTON_ROTARY, button_event);
         }
     }
 }
@@ -289,11 +304,11 @@ void EXTI9_5_IRQHandler(void)
 
         if (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_8))
         {
-            event_add_specific(&me.ev_hw_button, EVENT_BUTTON_RELEASE, (void *)INPUT_BUTTON_ROTARY, button_event);
+            event_add_specific(&me.ev_hw_button[INPUT_BUTTON_BACK], EVENT_BUTTON_RELEASE, (void *)INPUT_BUTTON_BACK, button_event);
         }
         else
         {
-            event_add_specific(&me.ev_hw_button, EVENT_BUTTON_PRESS, (void *)INPUT_BUTTON_ROTARY, button_event);
+            event_add_specific(&me.ev_hw_button[INPUT_BUTTON_BACK], EVENT_BUTTON_PRESS, (void *)INPUT_BUTTON_BACK, button_event);
         }
     }
 }
